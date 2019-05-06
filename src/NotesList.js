68fbf0e1c97d426191540d5b8013d20d62dd3754
Matt Raines <mraines4@gmail.com>
2019-05-06 10:39:12 -0400

    adds module styling

--- src/NotesList.js
+++ src/NotesList.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './NotesList.module.css';
 
 export default function NotesList(props) {
     return (
-        <ul>
+        <ul className={styles.list}>
             <li>This is the notes list!</li>
             <li>This is another note</li>
         </ul>
