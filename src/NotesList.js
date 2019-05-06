a723f96803ee63e1f909e57328df85f7475cd883
Matt Raines <mraines4@gmail.com>
2019-05-06 10:01:10 -0400

    adds notes application

---
+++ src/NotesList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotesList(props) {
+    return (
+        <ul>
+            <li>This is the notes list!</li>
+            <li>This is another note</li>
+        </ul>
+    )
+}
