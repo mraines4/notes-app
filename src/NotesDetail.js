a389d770ed73e39f58882eb586b99b42e5b1c573
Matt Raines <mraines4@gmail.com>
2019-05-06 11:52:58 -0400

    adds noteeditor

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -1,20 +1,34 @@
 import React from 'react';
 
+function NotesEditor({text}) {
+    return (
+        <textarea value={text} />
+    );
+}
+
 export default class NotesDetail extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            isEditing: true
         }
     }
     render() {
-        const {note} = this.props
+        const {note} = this.props;
+        const {isEditing} = this.state;
         return (
             <div>
                 <h1>{note.title}</h1>
-                <p>{note.text}</p>
+                {isEditing ? <NotesEditor text={note.text} /> : <p>{note.text}</p>}
+                <button >Edit</button>
             </div>
         )
     }
+
+    // _changeEdit = () => {
+    //     this.setState({
+    //         isEditing: !this.state.isEditing
+    //     })
+    // }
 }
