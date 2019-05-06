ac32b60af1e11dad591374231319aa9cab4f333b
Matt Raines <mraines4@gmail.com>
2019-05-06 14:21:16 -0400

    can now switch between notes and toggle notedetail

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -1,35 +1,49 @@
 import React from 'react';
 
-function NotesEditor({text}) {
+function NotesEditor({text, handleChange}) {
     return (
-        <textarea value={text} />
+        <textarea value={text} onChange={e => handleChange(e.target.value)} />
     );
 }
 
-export default class NotesDetail extends React.Component {
-    constructor(props) {
+export default class NotesDetail extends React.Component{
+    constructor(props){
         super(props);
-
         this.state = {
-            isEditing: false
+            isEditing: false,
+            draftText: props.note.text,
+            id: props.note.id
+        };
+    }
+    static getDerivedStateFromProps(props, state) {
+        if (props.note.id !== state.id) {
+            return {
+                id: props.note.id,
+                draftText: props.note.text
+            };
+        } else {
+            return null
         }
     }
-    render() {
+    render(){
         const {note} = this.props;
-        const {isEditing} = this.state;
-        return (
-            <div>
-                <h1>{note.title}</h1>
-                {isEditing ? <NotesEditor text={note.text} /> : <p>{note.text}</p>}
-                <br />
-                <button onClick={this._changeEdit}>Toggle</button>
-            </div>
+        const {isEditing, draftText} = this.state;
+        return(
+        <div>
+            <h2>{note.title}</h2> 
+            {isEditing? <NotesEditor text={draftText} handleChange={this._changeDraftText} /> :draftText }
+            <button onClick={this._toggleIsEditing}>Toggle</button>
+        </div>
         )
     }
-
-    _changeEdit = () => {
+    _toggleIsEditing = () => {
         this.setState({
             isEditing: !this.state.isEditing
+        })
+    }
+    _changeDraftText = (draftText) => {
+        this.setState({
+            draftText
         })
     }
 }
